a812c4e021595af9cad8cdc6c7d589429539f604
Dhruv Gramopadhye <dgramopadhye@gmail.com>
2025-05-23 21:31:14 -0700

    compiles! just needs correct memory

--- flight-controller/build.rs
+++ flight-controller/build.rs
@@ -31,6 +31,7 @@ fn main() {
 
     println!("cargo:rustc-link-arg-bins=--nmagic");
     println!("cargo:rustc-link-arg-bins=-Tlink.x");
-    println!("cargo:rustc-link-arg-bins=-Tlink-rp.x");
+// TODO(Dhruv) link-rp.x missing for pico2
+    // println!("cargo:rustc-link-arg-bins=-Tlink-rp.x");
     println!("cargo:rustc-link-arg-bins=-Tdefmt.x");
 }
--- flight-controller/src/main.rs
+++ flight-controller/src/main.rs
@@ -169,7 +169,10 @@ async fn main_inner(spawner: Spawner) -> Result<(), &'static str> {
     //     .spawn(logger_task(uart_tx))
     //     .map_err(|_| "failed to spawn logger task")?;
 
-    Timer::after_secs(2).await;
+    loop {
+        led.toggle();
+        Timer::after_secs(2).await;
+    }
 
     let mut armed = false;
     let mut trim_config: TrimConfig = TrimConfig::default();
